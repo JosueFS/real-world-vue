9588d7fed08aae6aa7c15cf2e14375a7a93923e6
JosueFS <josue.18rj@gmail.com>
2021-10-25 14:35:20 -0300

    Module 2 - Vue Mastery: Completed ✅🎉

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="icon-wrapper">
+    <v-icon class="icon" :name="name" scale="1" />
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { HiSolidUsers } from "oh-vue-icons/icons";
+
+addIcons(HiSolidUsers);
+
+export default {
+  props: {
+    //See additional props: https://oh-vue-icons.netlify.app/docs/#props
+    name: String,
+  },
+  components: {
+    "v-icon": OhVueIcon,
+  },
+};
+</script>
+
+<style>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  margin-right: 6px;
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+}
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Event List</h1>
+
+    <EventCard v-for="event in events" :key="event.id" :event="event">
+      {{ event }}
+    </EventCard>
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import api from "@/services/api.js";
+
+export default {
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [],
+    };
+  },
+  async created() {
+    try {
+      const response = await api.get("events");
+      this.events = response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
+
+<style></style>
